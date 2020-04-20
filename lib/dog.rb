00b2de661f0fd5b41378ef15b6714763e9763062
William Mittard <billymittard@ymail.com>
2020-04-20 15:13:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,12 @@
 class Dog 
-  def name 
-    fido.name = "Fido"
-  end 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+    end 
+    def 
+      
+    def bark 
+      puts "woof!"
+    end 
 end 
+
+fido
